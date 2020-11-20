6d950059c04225131e76050ad4645058eea73438
ishfaqmaknoo <ishfaqmaknoo12@gmail.com>
2020-11-21 03:43:40 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+
+const app = express()
+
+const importData = require('./data.json')
+
+let port = process.env.PORT || 3000;
+
+
+app.get('/' , (req,res) => {
+
+ res.send('Hello world')
+
+})
+
+app.get('/users' , (req,res) => {
+  res.send(importData);
+})
+
+
+app.listen(port , () => {
+
+  console.log(`Example app is listening on http://localhost:${port}`);
+
+})
+
